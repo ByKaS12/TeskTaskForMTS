5181316960fb6412e9e5745350e5c2be692a1a4f
Быков Егор <SEEGa1234@gmail.com>
2022-08-25 13:56:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TeskTaskForMTS
+{
+
+    internal class Program
+    {
+        public static void menu() {
+            Console.WriteLine("1. Ломай меня полностью.");
+            Console.WriteLine("2. Операция «Ы».");
+            Console.WriteLine("3. Мне только спросить!");
+            Console.WriteLine("4. Высший сорт.");
+            Console.WriteLine("5. Слон из мухи.");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        break;
+                    }
+                case "2":
+                    {
+                        break;
+                    }
+                case "3":
+                    {
+                        break;
+                    }
+                case "4":
+                    {
+                        break;
+                    }
+                case "5":
+                    {
+                        TestTask.Task_five();
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            menu();
+        }
+    }
+}
---
+++ TestTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeskTaskForMTS
+{
+    public static class TestTask
+    {
+        public static void TransformToElephant()
+        {
+            if (Console.Out.NewLine !="Слон")
+            {
+                Console.WriteLine("Слон");
+                Console.SetOut(new StringWriter() { NewLine = "Слон" });
+            }
+        }
+        public static void Task_five()
+        {
+            TransformToElephant();
+            Console.WriteLine("Муха");
+            //... custom application code
+
+        }
+    }
+}
